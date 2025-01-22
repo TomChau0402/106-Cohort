f4cea552186049b454fd450dba1f9c75b3ab6444
TomChau0402 <tung.t.t.chau@gmail.com>
2025-01-21 17:19:31 -0800

    assignment 3a

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,64 @@
-function sayHello() {
-    console.log("Hello");
-}
-function init() {
-    console.log("hello world");
-    sayHello();
-}
-window.onload = init
+function saveTask()
+{
+    console.log('saving task');
+    //get values
+    const title = $("#txtTitle").val();
+    const description = $("#txtDescription").val();
+    const color = $("#selColor").val();
+    const date = $("#selDate").val();
+    const status = $("#selStatus").val();
+    const budget = $("#numBudget").val();
+    console.log(title,description,color,date,status,budget);
+    //build an object
+    let taskToSave = new Task (title,description,color,date,status,budget);
+    console.log(taskToSave);
+    //save to server
 
-//variable scope
+    //display the data recieved from server
+    displayTask(taskToSave);
+}
+
+function displayTask(task)
+{
+    let syntax = `<div class='task'>
+    <div class='info'>
+        <h5>${task.title}</h5>
+        <p>${task.description}</p>
+    </div>
+    <label class='status'>${task.status}</label>
+    <div class='date-budget'>
+    <label>${task.date}</label>
+    <label>${task.budget}</label>
+    </div>
+    </div>
+    `;
+    $(".pending-tasks").append(syntax);
+}
+
+function testFuction()
+{
+    $.ajax({
+        type: "get",
+        url:"http://fsdiapi.azurewebsites.net",
+        success: function(response)
+        {
+            console.log(response);
+        },
+        error: function(error)
+        {
+            console.log(error);
+        }
+        
+    });
+}
+
+function init(){
+    console.log('init');
+    //load data
+
+    //hook events
+    $("#btnSave").click(saveTask);
+}
+
+window.onload = init;
+//Variable Scope
---
+++ scripts/task.js
@@ -0,0 +1,13 @@
+class Task {
+
+    constructor(title, x, color, date, status, budget)
+    {
+        this.title = title;
+        this.description = x;
+        this.color = color;
+        this.date = date;
+        this.status = status;
+        this.budget = budget;
+    }
+
+}
